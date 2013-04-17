d5db9fbb28bf519e746a7c435970045d4800bb32
Inez Foong <foong.inez@gmail.com>
2013-04-17 12:17:20 -0400

    Added race vectors and pop chloropleth

--- louisiana.R
+++ louisiana.R
@@ -1,3 +1,13 @@
 library(UScensus2010blkgrp)
 data(louisiana.blkgrp10)
-plot(louisiana.blkgrp10, xlim=c(-90.29, -89.84), ylim=c(29.81, 30.10))
+louisiana<-louisiana.blkgrp10
+plot(louisiana, xlim=c(-90.29, -89.84), ylim=c(29.81, 30.10))
+# Chloropleth of population
+choropleth(louisiana, xlim=c(-90.29, -89.84), ylim=c(29.81, 30.10))
+white<-louisiana$P0030002
+asian<-louisiana$P0030005
+black<-louisiana$P0030003
+native<-louisiana$P0030004
+hispanic<-louisiana$P0040001
+histogram(white)
+plot(louisiana, xlim=c(-90.29, -89.84), ylim=c(29.81, 30.10),col=white.col)
