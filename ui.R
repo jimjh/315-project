707668908f4532e727e894d74ac500ef420d1435
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2013-04-24 18:48:12 -0400

    updated UI

--- ui.R
+++ ui.R
@@ -1,22 +1,20 @@
 # install.packages('shiny')
 library(shiny)
 
-shinyUI(pageWithSidebar(
+shinyUI(bootstrapPage(
   
   headerPanel("US Census 2010"),
-  
-  # sidebar controls
-  sidebarPanel(
-    selectInput(inputId = "n_breaks",
-                label = "Number of bins in histogram (approximate):",
-                choices = c(10, 20, 35, 50),
-                selected = 20)
-  ),
   
   mainPanel(
     # multiple tabs
     tabsetPanel(
-      tabPanel('Plot', plotOutput('hist'))
+      tabPanel('Plot',
+               selectInput(inputId = "n_breaks",
+                           label = "Number of bins in histogram (approximate):",
+                           choices = c(10, 20, 35, 50),
+                           selected = 20),
+               plotOutput('hist')
+      )
     )
   )
   
